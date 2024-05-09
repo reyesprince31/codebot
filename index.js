bb970473fa108806ca72aa89b3c6291e7c142c34
Prince Reyes <77110899+reyesprince31@users.noreply.github.com>
2024-05-09 16:22:18 +0800

    feature new event handler

--- index.js
+++ index.js
@@ -7,12 +7,38 @@ export default (app) => {
   app.log.info("Yay, the app was loaded!");
 
   app.on("issues.opened", async (context) => {
+    //eventListener
     const issueComment = context.issue({
       body: "Thanks for opening this issue!",
     });
     return context.octokit.issues.createComment(issueComment);
   });
 
+  app.on("pull_request.opened", async (context) => {
+    const pull_request_title = context.payload.pull_request.title;
+    const pull_request_comment = context.payload.pull_request.body;
+    console.log(pull_request_title, pull_request_comment);
+
+    const prComment = {
+      owner: context.payload.repository.owner.login,
+      repo: context.payload.repository.name,
+      issue_number: context.payload.pull_request.number,
+      body: `Thanks for opening this pull request! this copy of ${pull_request_title}\n will be copied on local branch`,
+    };
+
+    return context.octokit.issues.createComment(prComment);
+  });
+
+  app.on("pull_request.synchronize", async (context) => {
+    const prComment = {
+      owner: context.payload.repository.owner.login,
+      repo: context.payload.repository.name,
+      issue_number: context.payload.pull_request.number,
+      body: `Thanks for this ${context.payload.pull_request.title}, I will update the local branch`,
+    };
+
+    return context.octokit.issues.createComment(prComment);
+  });
   // For more information on building apps:
   // https://probot.github.io/docs/
 
